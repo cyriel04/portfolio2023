8caac03e2036dd3490c2cccac64e0090247802e6
Cyriel Basilio <cbasilyow@gmail.com>
2023-09-11 20:52:25 +0800

    Added initial navigation

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navigation = () => {
+	return (
+		<nav id="navigation">
+			<div className="logo">Cyriel Basilio</div>
+			<ul>
+				<li>
+					<a href="#">About</a>
+				</li>
+				<li>
+					<a href="#">Experience</a>
+				</li>
+				<li>
+					<a href="#">Projects</a>
+				</li>
+				<li>
+					<a href="#">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navigation
