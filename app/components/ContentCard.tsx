58ed4eaf534605dd0dee88e660ec8f74a44bbfc9
Cyriel Basilio <cbasilyow@gmail.com>
2024-02-12 23:10:22 +0800

    Added mock colors

--- app/components/ContentCard.tsx
+++ app/components/ContentCard.tsx
@@ -6,10 +6,10 @@ const ContentCard = () => {
 	return (
 		<Card className={styles.contentCard}>
 			<CardContent>
-				<Typography gutterBottom variant="h6" component="div">
+				<Typography gutterBottom variant="h6" component="div" className={styles.title}>
 					Lorem Ipsum
 				</Typography>
-				<Typography variant="body2" color="text.secondary">
+				<Typography variant="body2">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tortor magna, auctor nec faucibus at, vulputate cursus
 					sapien. Phasellus tempor, tellus nec porttitor pellentesque, ex turpis convallis leo, sit amet interdum enim felis sit
 					amet diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas.
