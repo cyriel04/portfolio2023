ab697f5287ece8ae876ec0758385bd3952351220
Cyriel Basilio <cbasilyow@gmail.com>
2024-02-12 19:46:43 +0800

    Added mobile responsive

--- app/layout/Article.tsx
+++ app/layout/Article.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ContentCard from '../components/ContentCard'
+import styles from './Article.module.scss'
 
 const Article = () => {
 	return (
-		<div
-			className="content"
-			style={{
-				display: 'flex',
-				gap: 24
-			}}
-		>
+		<div className={styles.article}>
 			<ContentCard />
 			<ContentCard />
 			<ContentCard />
