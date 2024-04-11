e591f5c7812a7bbcc68583fe8e4a8e2f17978883
Cyriel Basilio <cbasilyow@gmail.com>
2024-04-11 13:08:03 +0800

    Added constant skillsList

---
+++ app/constants/index.ts
@@ -0,0 +1,46 @@
+export const skillsList = [
+	{
+		key: 1,
+		name: 'HTML'
+	},
+	{
+		key: 2,
+		name: 'CSS'
+	},
+	{
+		key: 3,
+		name: 'React'
+	},
+	{
+		key: 4,
+		name: 'Typescript'
+	},
+	{
+		key: 5,
+		name: 'NextJS'
+	},
+	{
+		key: 6,
+		name: 'SCSS'
+	},
+	{
+		key: 7,
+		name: 'Material UI'
+	},
+	{
+		key: 8,
+		name: 'Git'
+	},
+	{
+		key: 9,
+		name: 'Jest'
+	},
+	{
+		key: 10,
+		name: 'Storybook'
+	},
+	{
+		key: 10,
+		name: 'Javascript'
+	}
+]
